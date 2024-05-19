73f942ee6acdfab10fd5017a14f090446eb62408
Mary <114146302+1xiaoarui@users.noreply.github.com>
2024-05-19 11:27:26 +0800

    Update KuWoMusicNew.js

--- KuWoMusicNew.js
+++ KuWoMusicNew.js
@@ -9,9 +9,9 @@
 #!date = 2024-05-16
 #!select = 选择试听音质,至臻音质,无损音质,超品音质
 
-[Rule]
-USER-AGENT,KWPlayer*,PROXY
-HOST-SUFFIX,kuwo.cn,PROXY
+#[Rule]
+#USER-AGENT,KWPlayer*,PROXY
+#HOST-SUFFIX,kuwo.cn,PROXY
 
 [Script]
 http-response ^(?!.*img).*?kuwo\.cn(/vip|/openapi)?(/enc|/audi.tion|/v[\d]/(user/vip\?(vers|apiVersion|platform|op\=ui|_t)|theme\?op=gd|sysinfo\?op=getRePayAndDoPayBoxNew|api(/pay)?/((user/personal/)?user/info|payInfo/kwplayer/payMiniBar|advert/(myPage|iListen|album))|album/(adBar|myRec/vipMusic))|/kuwopay/vip-tab/setting|/(audioApi/)?a\.p($|\?op\=getvip|.*?ptype\=vip)|/mobi\.s\?f\=kwxs|/music\.pay\?newver\=3$|/(EcomResource|(Mobile)?Ad)Serv(er|ice)) script-path=https://napi.ltd/script/Worker/KuWo.js, requires-body=true, timeout=60, tag=酷我音乐, img-url=https://file.napi.ltd/Static/Image/KuWo.png
